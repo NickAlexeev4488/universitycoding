388619543d90e8a1207c8ec9c243563b2eea4566
alexluchsiy <N.Alexeev@seventest.ru>
2023-11-15 02:34:47 +0300

    I did it 6

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -137,9 +137,9 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    if find_empty_positions(grid) == (10, 10) or find_empty_positions(grid) is None:
-        return grid
     loc = find_empty_positions(grid)
+    if find_empty_positions(grid) == (10, 10):
+        return grid
     row, col = loc
 
     for num in find_possible_values(grid, (row, col)):
