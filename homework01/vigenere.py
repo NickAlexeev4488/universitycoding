a87b59d9fc520ca0a8a716f38dd3b6b1b9cbd040
alexluchsiy <N.Alexeev@seventest.ru>
2023-11-15 11:14:19 +0300

    fixed3

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -21,9 +21,9 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
         shift = ord(keyword[k]) % ord("A") if ord("A") <= ord(keyword[k]) <= ord("Z") else ord(keyword[k]) % ord("a")
         if ord("A") <= ord(letter) <= ord("Z"):
             if ord(letter) + shift > ord("Z"):
-                ciphertext += chr((ord(i) + shift) % ord("Z") + ord("A") - 1)
+                ciphertext += chr((ord(letter) + shift) % ord("Z") + ord("A") - 1)
             else:
-                ciphertext += chr(ord(i) + shift)
+                ciphertext += chr(ord(letter) + shift)
         elif ord("a") <= ord(letter) <= ord("z"):
             if ord(letter) + shift > ord("z"):
                 ciphertext += chr((ord(letter) + shift) % ord("z") + ord("a") - 1)
@@ -48,26 +48,24 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = ""
 
     if len(keyword) < len(ciphertext):
-        value_length = 0
-        keyword_length = len(keyword)
-        for i in ciphertext:
-            if value_length >= keyword_length:
-                keyword += keyword[value_length % keyword_length]
-            value_length += 1
+        key_length = len(keyword)
+        for i in range(len(ciphertext)):
+            if i >= key_length:
+                keyword += keyword[i % key_length]
 
     for k in range(len(ciphertext)):
-        i = ciphertext[k]
+        letter = ciphertext[k]
         shift = ord(keyword[k]) % ord("A") if ord("A") <= ord(keyword[k]) <= ord("Z") else ord(keyword[k]) % ord("a")
         if ord("A") <= ord(i) <= ord("Z"):
-            if ord(i) - shift < ord("A"):
-                plaintext += chr(ord(i) - shift + ord("Z") - ord("A"))
+            if ord(letter) - shift < ord("A"):
+                plaintext += chr(ord(letter) - shift + ord("Z") - ord("A"))
             else:
-                plaintext += chr(ord(i) - shift)
-        elif ord("a") <= ord(i) <= ord("z"):
-            if ord(i) - shift < ord("a"):
-                plaintext += chr(ord(i) - shift + ord("z") - ord("a"))
+                plaintext += chr(ord(letter) - shift)
+        elif ord("a") <= ord(letter) <= ord("z"):
+            if ord(letter) - shift < ord("a"):
+                plaintext += chr(ord(letter) - shift + ord("z") - ord("a"))
             else:
-                plaintext += chr(ord(i) - shift)
+                plaintext += chr(ord(letter) - shift)
         else:
             plaintext += i
 
