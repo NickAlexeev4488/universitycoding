57d7773d34b6bb4e37222f4c645dea2e1b44d5d4
Nikolai Alexeev <N.Alexeev@seventest.ru>
2024-06-13 14:32:35 +0300

    all done

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,5 +1,6 @@
-import numpy as np
 from math import log as ln
+
+import numpy as np
 
 
 class NaiveBayesClassifier:
--- homework07/research/vkapi/config.py
+++ homework07/research/vkapi/config.py
@@ -1,5 +1,6 @@
 VK_CONFIG = {
     "domain": "https://api.vk.com/method/",
-    "access_token": "",
+    "access_token": "vk1.a"
+                    ".ZIYIx0avOMEIAn7jzsXU6IucDSbRff6GNniLymWXRkzqTK8nNzUICpw2Fnlh135AXFdLntnAtI_lLihMQIUlpxrF_pb89CCCZtozS0wE3ol310GsrOpKzBTa9YKRB4k-9huoqCh5xckNNTvkwK9_1T-nIOdZfCicFGEt_G6b9nsUkBkGjYKco0AAeC1KZJ2B",
     "version": "5.126",
 }
