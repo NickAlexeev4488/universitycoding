7fe91add75869d10f70a20772b2a22cf0f4d4b8d
Nikolai Alexeev <N.Alexeev@seventest.ru>
2024-06-13 15:08:08 +0300

    all done

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,9 +1,9 @@
 import string
 
-from bayes import NaiveBayesClassifier
+from homework06.bayes import NaiveBayesClassifier
 from bottle import redirect, request, route, run, template
-from db import News, session
-from scraputils import get_news
+from homework06.db import News, session
+from homework06.scraputils import get_news
 
 
 def clean(s):
